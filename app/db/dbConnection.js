a3cc4cfb5862977cdf468740a03d4056cfeb58b5
magdielmarques <contato.magdiel@gmail.com>
2023-02-01 19:45:57 -0400

    feat: query to create table when connected

--- app/db/dbConnection.js
+++ app/db/dbConnection.js
@@ -6,10 +6,18 @@ const dbConnection = () => {
 
   connection.connect((error) => {
     if (error) {
-      console.log("Database Connection Failed !!!", error);
+      console.log("Database Connection Failed!", error);
     } else {
-      console.log("connected to Database");
+      console.log("Connected to Database!");
     }
+  });
+
+  const tableQuery =
+    "CREATE TABLE people (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))";
+
+  connection.query(tableQuery, (error) => {
+    if (error) console.log(`Table People not created: \n ${error}`);
+    else console.log(`Successfully created table People`);
   });
 };
 
