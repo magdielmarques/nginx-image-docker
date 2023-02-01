fbb55c51b7770f30cb8cfec12e41e0e08c5211f2
magdielmarques <contato.magdiel@gmail.com>
2023-01-31 21:50:03 -0400

    feat: set up nginx proxy revers

--- app/server.js
+++ app/server.js
@@ -6,14 +6,13 @@ const server = () => {
   const PORT = 3000;
 
   application.get("/", (req, res) => {
-    res.send("<h1>Full Cycle Rocks!</h1>");
+    insertPeople();
+    res.send(`<h1>Full Cycle Rocks!</h1> <br />`);
   });
 
   application.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-
-  // insertPeople();
 };
 
 module.exports = server;
